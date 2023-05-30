8e7f1198b80f6e7e4012153e11ac03f945c24b85
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-05-30 22:26:38 +0200

    update doxckerfile

--- docker/platform.sh
+++ docker/platform.sh
@@ -3,18 +3,21 @@
 # Used in Docker build to set platform dependent variables
 
 case $TARGETARCH in
-
-    "amd64")
-	echo "x86_64-unknown-linux-gnu" > /.platform
-	echo "" > /.compiler 
+	"amd64")
+		echo "x86_64-unknown-linux-gnu" > /.platform
+		echo "clang" > /.compiler
+		curl https://github.com/rui314/mold/releases/download/v1.11.0/mold-1.11.0-x86_64-linux.tar.gz -o mold.tar.gz
+		tar -xzf mold.tar.gz
+		mv mold/* /
+		rm -rf mold mold.tar.gz
 	;;
-    "arm64") 
-	echo "aarch64-unknown-linux-gnu" > /.platform
-	echo "gcc-aarch64-linux-gnu" > /.compiler
+	"arm64")
+		echo "aarch64-unknown-linux-gnu" > /.platform
+		echo "gcc-aarch64-linux-gnu" > /.compiler
 	;;
-    "arm")
-	echo "armv7-unknown-linux-gnueabihf" > /.platform
-	echo "gcc-arm-linux-gnueabihf" > /.compiler
+	"arm")
+		echo "armv7-unknown-linux-gnueabihf" > /.platform
+		echo "gcc-arm-linux-gnueabihf" > /.compiler
 	;;
 esac
 
