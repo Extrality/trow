8b3d859a83abc2a27ab9c925e9414dfea38ea5c9
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-05-31 01:54:24 +0200

    really fix docker builds

--- docker/platform.sh
+++ docker/platform.sh
@@ -6,10 +6,11 @@ case $TARGETARCH in
 	"amd64")
 		echo "x86_64-unknown-linux-gnu" > /.platform
 		echo "clang" > /.compiler
-		curl https://github.com/rui314/mold/releases/download/v1.11.0/mold-1.11.0-x86_64-linux.tar.gz -o mold.tar.gz
+		mold_arch="x86_64"
+		curl -L https://github.com/rui314/mold/releases/download/v1.11.0/mold-1.11.0-x86_64-linux.tar.gz -o mold.tar.gz
 		tar -xzf mold.tar.gz
-		mv mold/* /
-		rm -rf mold mold.tar.gz
+		cp -rl mold*/* /usr
+		rm -rf mold*
 	;;
 	"arm64")
 		echo "aarch64-unknown-linux-gnu" > /.platform
@@ -17,7 +18,7 @@ case $TARGETARCH in
 	;;
 	"arm")
 		echo "armv7-unknown-linux-gnueabihf" > /.platform
-		echo "gcc-arm-linux-gnueabihf" > /.compiler
+		echo "gcc-arm-linux-gnuabihf" > /.compiler
 	;;
 esac
 
